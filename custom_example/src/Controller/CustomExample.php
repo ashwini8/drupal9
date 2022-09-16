328dfe09c68e716ed53b7bbfe5944f382fab58f5
ashwini8 <ashwini.kadam2606@gmail.com>
2022-09-16 17:56:50 +0530

    Add files via upload

--- custom_example/src/Controller/CustomExample.php
+++ custom_example/src/Controller/CustomExample.php
@@ -1,1 +1,14 @@
-
+<?php
+/**
+ * @file
+ * Contains \Drupal\hello_world\Controller\HelloController.
+ */
+namespace Drupal\custom_example\Controller;
+class CustomExample {
+  public function content() {
+    return array(
+      '#type' => 'markup',
+      '#markup' => t('Hello, World!'),
+    );
+  }
+}
